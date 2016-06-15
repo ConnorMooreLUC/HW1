a855d36add5ac16911845ad6e3af44f3d039871a
Connor Moore <cmoore12@luc.edu>
2016-06-15 05:31:11 -0500

    Converted to DictReader for simplicity. Comparisons Imminent.
    My next task will be comparing the two sets of data.

--- Library Statistics.py
+++ Library Statistics.py
@@ -1,25 +1,36 @@
 import csv
 
+library13={}
+library14={}
+
 f = open('Library_2013.csv')
 
-csv_f = csv.reader(f)
+csv_f = csv.DictReader(f)
 
-f.readline()  #skips firstline
+#f.readline()  #skips firstline,  not neccessary with DictReader
 
 for row in csv_f:
-    print(row[0],"   ",row[1],"   ",row[16],"\n")
+    #print(row[0],"   ",row[1],"   ",row[16],"\n")
+    print(row['LOCATION'],row['YTD'])
+    library13[row['LOCATION']] = row['YTD']
 
 f.close()
 
 
 
+
 g = open('Library_2014.csv')
 
-csv_g = csv.reader(g)
+csv_g = csv.DictReader(g)
 
-g.readline()  #skips firstline
+#g.readline()  #skips firstline, not neccessary with DictReader
 
 for row in csv_g:
-    print(row[0],"   ",row[1],"   ",row[16],"\n")
+    print(row['LOCATION'],row['YTD'])
+    library14[row['LOCATION']] = row['YTD']
 
 g.close()
+
+
+#print(library13.items(),"\n\n\n")
+#print(library14.items())
