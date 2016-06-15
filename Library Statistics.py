e53fdd6e4d59a709ce8c5656e618103f6c5e6afd
Connor Moore <cmoore12@luc.edu>
2016-06-15 04:15:12 -0500

    Add files via upload

--- Library Statistics.py
+++ Library Statistics.py
@@ -4,7 +4,22 @@ f = open('Library_2013.csv')
 
 csv_f = csv.reader(f)
 
+f.readline()  #skips firstline
+
 for row in csv_f:
-    print(row[16])
+    print(row[0],"   ",row[1],"   ",row[16],"\n")
 
 f.close()
+
+
+
+g = open('Library_2014.csv')
+
+csv_g = csv.reader(g)
+
+g.readline()  #skips firstline
+
+for row in csv_g:
+    print(row[0],"   ",row[1],"   ",row[16],"\n")
+
+g.close()
