ef792d889ff1e3d7729245119e4b6684b586a719
Connor Moore <cmoore12@luc.edu>
2016-06-15 06:08:05 -0500

    Add files via upload

--- Library Statistics.py
+++ Library Statistics.py
@@ -2,6 +2,9 @@ import csv
 
 library13={}
 library14={}
+namelist=[]
+liblist13=[]
+liblist14=[]
 
 f = open('Library_2013.csv')
 
@@ -10,11 +13,13 @@ csv_f = csv.DictReader(f)
 #f.readline()  #skips firstline,  not neccessary with DictReader
 
 for row in csv_f:
-    #print(row[0],"   ",row[1],"   ",row[16],"\n")
     print(row['LOCATION'],row['YTD'])
     library13[row['LOCATION']] = row['YTD']
-
+    liblist13.append(row['YTD'])
+    namelist.append(row['LOCATION'])
+    
 f.close()
+print("\n\n\n")
 
 
 
@@ -28,9 +33,14 @@ csv_g = csv.DictReader(g)
 for row in csv_g:
     print(row['LOCATION'],row['YTD'])
     library14[row['LOCATION']] = row['YTD']
+    liblist14.append(row['YTD'])
 
 g.close()
 
+for index in range(len(liblist13)):
+    print(namelist[index], "2013: ",
+          liblist13[index],"2014: ",liblist14[index],"  % change: "
+          ,100*(int(liblist14[index]) - int(liblist13[index]))/int(liblist13[index]))
 
 #print(library13.items(),"\n\n\n")
 #print(library14.items())
