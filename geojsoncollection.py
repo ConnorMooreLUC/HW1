b8d1cea4a0558ac4412349d25628306c6bc4bba6
Connor Moore <cmoore12@luc.edu>
2016-06-27 04:16:07 -0500

    Finally cracked the GIS wall. Will complete GIS methods tomorrow.

--- geojsoncollection.py
+++ geojsoncollection.py
@@ -4,42 +4,37 @@ Created on Wed Jun 22 02:08:37 2016
 
 @author: galli_000
 """
-from geopy.geocoders import Nominatim
-import json
-from shapely.geometry import Point, MultiPolygon
+from geopy.geocoders import GoogleV3
+from shapely.geometry import Point, shape
 import os
 import pygeoj
 
 
 os.chdir('C:\\Users\\galli_000\\Desktop\\gitfolder\\HW1')
-geolocator = Nominatim()
-location = geolocator.geocode("1210 W. Elmdale Avenue Chicago")
+geolocator = GoogleV3()
+location = geolocator.geocode('1032 Sheridan Rd Chicago')
 print(location.address)
 print(location.latitude, location.longitude)
 
-point = Point(location.latitude, location.longitude)
-#with open('bounds.json') as f:
-#    js = json.load(f)
-#    print(js.keys())
-#    for item in js['data']:
-#        print(type(item))
-#        item.sort()
-#        for entry in item:
-#            print(item)
+point = Point(location.longitude, location.latitude)
+point2 = Point(location.latitude, location.longitude)
 
 testfile = pygeoj.load("bounds.geojson")
 for feature in testfile:
-    print(feature.geometry.type)
-    print(feature.geometry.coordinates)
-    #sample = MultiPolygon(feature.geometry.coordinates)
-    #print(type(sample))
-    #print(sample.contains(point))
-    #print(sample)            
-
-#js = pygeoj.load('bounds.geojson')
-#for feature in js:
-#    multipoly = shape(feature.geometry.coordinates)
-#    if multipoly.contains(point):
-#        print('Found containing polygon:', feature)
-#    #print(feature,feature.geometry.coordinates)
-        
+    #print(feature.geometry.type)
+    #print(testfile.common_attributes)
+    #print(feature.properties['pri_neigh'])
+    #print(feature.geometry)
+    #print(multipoly)
+    #print('\nSuccess\n')
+    multipoly = shape(feature.geometry)
+    if multipoly.contains(point):
+        print('woo',feature.properties['pri_neigh'])
+        print(multipoly.bounds)
+    elif multipoly.contains(point2):
+        print('wippee')
+    elif multipoly.bounds.__contains__(point):
+        print('Succes; in neighborhood: ', feature.properties['pri_neigh'])
+    elif multipoly.bounds.__contains__(point2):
+        print('Yay', feature.properties['pri_neigh'])
+             
